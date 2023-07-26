8dd0bb4f208f3ff0f7b001dcb31d1cde893d8697
bharatgulati <bharat.gulati@cloudsufi.com>
2023-07-26 23:21:33 +0530

    BigQuery MultiTable Validation

--- src/e2e-test/java/io/cdap/plugin/bigquerymultitable/Actions/BQMTActions.java
+++ src/e2e-test/java/io/cdap/plugin/bigquerymultitable/Actions/BQMTActions.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package io.cdap.plugin.bigquerymultitable.actions;
+package io.cdap.plugin.bigquerymultitable.Actions;
 
 import io.cdap.e2e.utils.ElementHelper;
 import io.cdap.e2e.utils.SeleniumHelper;
